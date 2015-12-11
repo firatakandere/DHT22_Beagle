cfd45229bd9ea92ec70556767937c659c679963f
firat <f.akandere@gmail.com>
2015-12-11 13:08:05 +0200

    Extended documentation

--- Beagle_GPIO_dht22.cpp
+++ Beagle_GPIO_dht22.cpp
@@ -1,12 +1,5 @@
 #include "DHT22_Beagle/Beagle_GPIO_dht22.h"
 
-/**
- * @brief bbb_read_dht22 Read DHT22 Data From BeagleBone Black
- * @param pin Pin number, check Beagle_GPIO, GPIO Pins
- * @param temperature Address of the temperature variable where the temperature value to be written
- * @param humidity Address of the humidity variable where the humidity value to be written
- * @return Returns zero if successful. Returns -1 if the checksum for data is failed.
- */
 int bbb_read_dht22(unsigned short pin, float *temperature, float *humidity) {
     Beagle_GPIO	gpio;
     int counter = 0;
@@ -88,12 +81,6 @@ int bbb_read_dht22(unsigned short pin, float *temperature, float *humidity) {
     return 0;
 }
 
-/**
- * @brief diff Compute diff for timespec from clock_gettime
- * @param start Starting time
- * @param end Ending time
- * @return Time difference
- */
 timespec diff(timespec start, timespec end)
 {
     timespec temp;
--- Beagle_GPIO_dht22.h
+++ Beagle_GPIO_dht22.h
@@ -9,7 +9,21 @@
 
 #define MAXTIMINGS 84
 
+/**
+ * @brief diff Compute diff for timespec from clock_gettime
+ * @param start Starting time
+ * @param end Ending time
+ * @return Time difference
+ */
 timespec diff(timespec start, timespec end);
+
+/**
+ * @brief bbb_read_dht22 Read DHT22 Data From BeagleBone Black
+ * @param pin Pin number, check Beagle_GPIO, GPIO Pins
+ * @param temperature Address of the temperature variable where the temperature value to be written
+ * @param humidity Address of the humidity variable where the humidity value to be written
+ * @return Returns zero if successful. Returns -1 if the checksum for data is failed.
+ */
 int bbb_read_dht22(unsigned short pin, float *temperature, float *humidity);
 
 #endif // BEAGLE_GPIO_DHT22_H
